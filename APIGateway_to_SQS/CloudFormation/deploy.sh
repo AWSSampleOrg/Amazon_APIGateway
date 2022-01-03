6eb0a244dde5629d17a7681b0647622ec8aa5805
Yo-mah-Ya <59109232+Yo-mah-Ya@users.noreply.github.com>
2022-01-03 17:50:52 +0900

    Add files via upload

--- APIGateway_to_SQS/CloudFormation/deploy.sh
+++ APIGateway_to_SQS/CloudFormation/deploy.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
-StackName="APIGateway-to-SQS"
+#!/usr/bin/env bash
+CFN_STACK_NAME="APIGateway-to-SQS"
+
+SOURCE_DIR=$(cd $(dirname ${BASH_SOURCE:-0}) && pwd)
+cd ${SOURCE_DIR}
 
 #deploy
 aws cloudformation deploy \
     --template-file template.yml \
-    --stack-name ${StackName} \
+    --stack-name ${CFN_STACK_NAME} \
     --parameter-overrides \
         APIGatewayStageName=test \
-    --capabilities CAPABILITY_IAM
+    --capabilities CAPABILITY_NAMED_IAM
