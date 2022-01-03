ad56ebc393f3a6a4f8566d2b7180b0e5f261f83d
Yo-mah-Ya <59109232+Yo-mah-Ya@users.noreply.github.com>
2022-01-03 17:48:29 +0900

    Add files via upload

--- APIGatewayMock/CloudFormation/deploy.sh
+++ APIGatewayMock/CloudFormation/deploy.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
-StackName="APIGatewayMock"
+#!/usr/bin/env bash
+CFN_STACK_NAME="APIGatewayMock"
+
+SOURCE_DIR=$(cd $(dirname ${BASH_SOURCE:-0}) && pwd)
+cd ${SOURCE_DIR}
 
 #deploy
 aws cloudformation deploy \
     --template-file template.yml \
-    --stack-name ${StackName} \
+    --stack-name ${CFN_STACK_NAME} \
     --parameter-overrides \
         APIGatewayStageName=test \
-   --capabilities CAPABILITY_IAM
+   --capabilities CAPABILITY_NAMED_IAM
