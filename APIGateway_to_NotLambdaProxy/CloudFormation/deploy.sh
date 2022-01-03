31bb7c0d7e17e183a3c429c205c22b4a54e8524a
Yo-mah-Ya <59109232+Yo-mah-Ya@users.noreply.github.com>
2022-01-03 17:50:22 +0900

    Add files via upload

--- APIGateway_to_NotLambdaProxy/CloudFormation/deploy.sh
+++ APIGateway_to_NotLambdaProxy/CloudFormation/deploy.sh
@@ -1,17 +1,20 @@
-#!/bin/bash
-readonly S3_Bucket="your-bucket-name" 2> /dev/null
-StackName="APIGateway-to-NotLambdaProxy"
+#!/usr/bin/env bash
+S3_BUCKET="your-bucket-name"
+CFN_STACK_NAME="APIGateway-to-NotLambdaProxy"
+
+SOURCE_DIR=$(cd $(dirname ${BASH_SOURCE:-0}) && pwd)
+cd ${SOURCE_DIR}
 
 #package
 aws cloudformation package \
     --template-file template.yml \
-    --s3-bucket ${S3_Bucket} \
+    --s3-bucket ${S3_BUCKET} \
     --output-template-file packaged_template.yml
 
 #deploy
 aws cloudformation deploy \
     --template-file packaged_template.yml \
-    --stack-name ${StackName} \
+    --stack-name ${CFN_STACK_NAME} \
     --parameter-overrides \
         APIGatewayStageName=test \
-   --capabilities CAPABILITY_IAM
+   --capabilities CAPABILITY_NAMED_IAM
